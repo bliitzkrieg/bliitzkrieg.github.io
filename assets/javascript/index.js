984124e59ccc8de58ec86f4a541b039d92febf58
Luca DeCaprio <lucamatthewdecaprio@gmail.com>
2016-04-06 19:40:01 -0400

    Update 2016-04-06T23:40:01.076Z

---
+++ assets/javascript/index.js
@@ -0,0 +1,4 @@
+
+console.log('%c Hey There! Glad to see you down here. This site is built using Jekyll, SASS and gulp and hosted on github pages.\n You can see the code here https://github.com/bliitzkrieg/bliitzkrieg.github.io', 'background: #222; color: #bada55');
+
+//# sourceMappingURL=index.js.map
